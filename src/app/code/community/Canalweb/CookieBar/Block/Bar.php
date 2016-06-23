924c0dac5cd0b2069719290ecdc57f9c89ab3572
Fwagra <fabiocerqueiracw@gmail.com>
2016-06-23 12:16:24 +0200

    Initial commit

---
+++ src/app/code/community/Canalweb/CookieBar/Block/Bar.php
@@ -0,0 +1,4 @@
+<?php
+class Canalweb_CookieBar_Block_Bar extends Mage_Catalog_Block_Product_Abstract
+{
+}
---
+++ src/app/code/community/Canalweb/CookieBar/Model/System/Config/Source/Dropdown/Values.php
@@ -0,0 +1,18 @@
+<?php
+
+class Canalweb_CookieBar_Model_System_Config_Source_Dropdown_Values
+{
+    public function toOptionArray()
+    {
+        return array(
+            array(
+                'value' => 'haut',
+                'label' => 'Haut',
+            ),
+            array(
+                'value' => 'bas',
+                'label' => 'Bas',
+            ),
+        );
+    }
+}
